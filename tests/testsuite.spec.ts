131ffd694bdea42677ecd53fe08f9e91ab939828
Jesper <jesper@flygar.se>
2024-10-08 19:21:25 +0200

    working on login for front and back video

--- tests/testsuite.spec.ts
+++ tests/testsuite.spec.ts
@@ -1,8 +1,26 @@
 import { test, expect } from '@playwright/test';
 
+test.describe('Frontend tests', () => {
+    test('Create a client', async ({ page }) => {
+        await page.goto('http://localhost:3000');
+        await page.locator('input[type="text"]').fill(`${process.env.TEST_USERNAME}`);
+        await page.locator('input[type="password"]').fill(`${process.env.TEST_PASSWORD}`);
+        await page.getByRole('button', { name: 'Login' }).click(); 
+        await expect(page.getByRole('heading', { name: 'Tester Hotel Overview' })).toBeVisible(); 
+        // rest of code
+    });
+});
+
+
 test.describe('Backend tests', () => {
-    test('Get all posts', async ({ request }) => {
-        const response = await request.get('http://localhost:3000/posts');
+    test('Create a client', async ({ request }) => {
+        const response = await request.post('http://localhost:3000/api/login', {
+            data: {
+                "username": `${process.env.TEST_USERNAME}`,
+                "password": `${process.env.TEST_PASSWORD}`
+            }
+        });
         expect(response.ok()).toBeTruthy(); 
-    })
-})
+        // rest of code
+    });
+});
