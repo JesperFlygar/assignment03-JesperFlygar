60e90ec68f4d74f8b18e7fc1c0535b6756054ca7
Jesper <jesper@flygar.se>
2024-10-09 21:57:30 +0200

    potential files for create front and back, also added text to create both not implemented

--- tests/testsuite.spec.ts
+++ tests/testsuite.spec.ts
@@ -1,4 +1,5 @@
 import { test, expect } from '@playwright/test';
+import { faker } from '@faker-js/faker';
 
 test.describe('Frontend tests', () => {
     test('Create a client', async ({ page }) => {
@@ -8,7 +9,27 @@ test.describe('Frontend tests', () => {
         await page.locator('input[type="password"]').fill(`${process.env.TEST_PASSWORD}`);
         await page.getByRole('button', { name: 'Login' }).click(); 
         await expect(page.getByRole('heading', { name: 'Tester Hotel Overview' })).toBeVisible(); 
-        // rest of code
+        
+        /*const viewButton = page.locator('#app > div > div > div:nth-child(2) > a');
+        await viewButton.click(); 
+        await expect(page.getByRole('link', { name: 'Create Client' })).toBeVisible();
+
+        const createButton = page.getByRole('link', { name: 'Create Client' });
+        await createButton.click();
+        
+        const fullName = faker.person.fullName();
+        const userEmail = faker.internet.email();
+        const userPhoneNo = faker.phone.number();
+        
+        await page.locator('div').filter({ hasText: /^Name$/ }).getByRole('textbox').fill(fullName);
+        await page.locator('input[type="email"]').fill(userEmail);
+        await page.locator('div').filter({ hasText: /^Telephone$/ }).getByRole('textbox').fill(userPhoneNo);
+        await page.getByText('Save').click();
+        return page.locator('#app > div > div.clients > div:nth-last-child(1)');*/
+        
+        /*await expect(element).toContainText(fullName);
+        await expect(element).toContainText(userEmail);
+        await expect(element).toContainText(userPhoneNo);*/
     });
 });
 
@@ -23,6 +44,18 @@ test.describe('Backend tests', () => {
             }
         });
         expect(response.ok()).toBeTruthy(); 
-        // rest of code
+        /*async createPost(request: APIRequestContext, target: string, payload: object) {
+        const response = await request.post(`${this.baseURL}/api/${target}/new`, {
+            data: JSON.stringify(payload),
+            headers: this.getHeader()
+        })
+        return response;
+        }
+        name: faker.person.fullName(),
+        email: faker.internet.email(),
+        telephone: faker.phone.number()
+        
+        
+        expect....*/
     });
 });
